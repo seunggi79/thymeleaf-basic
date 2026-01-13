db8f614805aca20f402e5c5b7f8122ee36c8aab2
seunggi <hsg77689469.gamil.com>
2026-01-13 14:38:03 +0900

    반복

--- src/main/java/hello/thymeleaf_basic/basic/BasicController.java
+++ src/main/java/hello/thymeleaf_basic/basic/BasicController.java
@@ -99,4 +99,16 @@ public class BasicController {
     public String attribute() {
         return "basic/attribute";
     }
+    @GetMapping("/each")
+    public String each(Model model) {
+        addUsers(model);
+        return "basic/each";
+    }
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+        model.addAttribute("users", list);
+    }
 }
